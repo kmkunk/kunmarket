634471fdb7771b0e6cf1f47acb1bd55acfe04510
myoungkun_kim <slb09@naver.com>
2022-07-13 01:49:17 +0900

    Apply duplicateCheckEmail validation and add exception

--- src/main/java/com/kmkunk/kunmarket/users/UserRepository.java
+++ src/main/java/com/kmkunk/kunmarket/users/UserRepository.java
@@ -3,4 +3,6 @@ package com.kmkunk.kunmarket.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/kmkunk/kunmarket/users/UserService.java
+++ src/main/java/com/kmkunk/kunmarket/users/UserService.java
@@ -1,7 +1,11 @@
 package com.kmkunk.kunmarket.users;
 
+import com.kmkunk.kunmarket.common.exception.DuplicateException;
 import com.kmkunk.kunmarket.users.dto.UserRequestDto;
+import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +15,17 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void signUp(UserRequestDto userRequestDto) {
+        //중복확인 및 밸리데이션 해야함
         //비멀번호 암호화 해야함
-        userRepository.save(userRequestDto.toEntity());
+        if (duplicateCheckEmail(userRequestDto.getEmail())) {
+            userRepository.save(userRequestDto.toEntity());
+        }
+    }
+
+    public boolean duplicateCheckEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new DuplicateException("이미 등록된 이메일입니다.");
+        }
+        return true;
     }
 }
