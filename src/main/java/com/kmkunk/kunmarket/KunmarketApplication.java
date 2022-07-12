d2338cbad0a6dbb9ed2789540111028ab850d52c
myoungkun_kim <slb09@naver.com>
2022-07-12 16:04:01 +0900

    Initial commit

---
+++ src/main/java/com/kmkunk/kunmarket/KunmarketApplication.java
@@ -0,0 +1,13 @@
+package com.kmkunk.kunmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunmarketApplication.class, args);
+	}
+
+}
